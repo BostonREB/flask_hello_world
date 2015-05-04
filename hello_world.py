fb8644123aa5501d5673e9eb52cfb0f187c8bd5e
Rich Bowman <BostonREB99@gmail.com>
2015-05-04 15:18:31 -0400

    Add in templating

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route("/")
+def template_test():
+    return render_template('template.html', my_string="This is my STRING!!", my_list=[0,1,2,3,4,5,6,7])
 
 @app.route("/hello")
 def say_hi():
@@ -19,6 +23,24 @@ def get_jedi_name(first_name, last_name):
     <p> Your Jedi name is: <b>{}</b></p>
   """
   return html.format(jedi_name.title())
+
+@app.route("/home")
+def home():
+    return render_template('template.html',
+                           my_string="I'm the home page",
+                           my_list=[0,1,2,3,4,5])
+
+@app.route("/about")
+def about():
+    return render_template('template.html',
+                            my_string="I'm the about page",
+                            my_list=[0,1,2,3,4,5])
+
+@app.route("/contact")
+def contact():
+    return render_template('template.html',
+                           my_string="I'm the contact page",
+                           my_list=[0,1,2,3,4,5])
 
 if __name__ == "__main__":
   app.run()
