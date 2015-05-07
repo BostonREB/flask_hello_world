46476a07445bddb820d6ea6bf883d465981cfbaa
Rich Bowman <BostonREB99@gmail.com>
2015-05-07 13:25:42 -0400

    add in jinja

--- hello_world.py
+++ hello_world.py
@@ -3,42 +3,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('template.html', my_string="This is my STRING!!", my_list=[0,1,2,3,4,5,6,7])
+    return render_template('template2.html', my_string="This is my STRING!!", my_list=[0,1,2,3,4,5,6,7])
 
 @app.route("/hello")
 def say_hi():
-  return "Hello World!"
+  return render_template('template2.html', my_string="Hello World!!")
 
 @app.route("/hello/<name>")
 def hi_person(name):
-  return "Hello {}!".format(name.title())
+  return render_template('template2.html', my_string="Hello {}!".format(name.title()))
 
 @app.route("/jedi/<first_name>/<last_name>")
 def get_jedi_name(first_name, last_name):
   jedi_name = last_name[0:3] + first_name[0:2]
-  html = """
-    <h1>
-      Hello!
-    </h1>
-    <p> Your Jedi name is: <b>{}</b></p>
-  """
-  return html.format(jedi_name.title())
+  return render_template('template2.html', my_string='Your Jedi Name is: {}'.format(jedi_name.title()))
 
 @app.route("/home")
 def home():
-    return render_template('template.html',
+    return render_template('template2.html',
                            my_string="I'm the home page",
                            my_list=[0,1,2,3,4,5])
 
 @app.route("/about")
 def about():
-    return render_template('template.html',
+    return render_template('template2.html',
                             my_string="I'm the about page",
                             my_list=[0,1,2,3,4,5])
 
 @app.route("/contact")
 def contact():
-    return render_template('template.html',
+    return render_template('template2.html',
                            my_string="I'm the contact page",
                            my_list=[0,1,2,3,4,5])
 
